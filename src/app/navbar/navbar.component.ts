b644b84cdce998af0b41f52454837a2940b03900
Gayatri Pisya <gayatripisya@gmail.com>
2024-10-01 23:46:51 +0530

    updated the nav bar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  // Property to track the state of the mobile menu
+  isMenuOpen: boolean = false;
 
+  // Method to toggle the mobile menu
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
