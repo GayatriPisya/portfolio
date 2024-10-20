207c2f8d7f9ed9f8062b939153d5ec09330e4e7b
Gayatri Pisya <gayatripisya@gmail.com>
2024-10-20 22:06:47 +0530

    updated

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,34 @@
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  public isSending: boolean = false; 
+  public confirmationMessage: string | null = null;
 
+  constructor() {
+    emailjs.init('u9ilDbudfHKFmiGQT'); 
+  }
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    this.isSending = true; // Set sending flag to true
+
+    emailjs.sendForm('service_052j5yu', 'template_y4gdfaz', e.target as HTMLFormElement, 'u9ilDbudfHKFmiGQT')
+      .then((result: EmailJSResponseStatus) => {
+        console.log('Email sent successfully:', result);
+        this.confirmationMessage = "Your message has been sent successfully!"; // Set confirmation message
+        (e.target as HTMLFormElement).reset(); // Reset the form
+      }, (error) => {
+        console.error('Error sending email:', error);
+        this.confirmationMessage = "There was an error sending your message. Please try again."; // Set error message
+      })
+      .finally(() => {
+        this.isSending = false; // Reset sending flag after process is complete
+      });
+  }
 }
